50c9f9e3d032c08f6363b9e871d0901d98b2bd22
rashu37 <rashmi10.xworkz@hmail.com>
2024-06-26 19:16:00 +0530

    26/6, Database

---
+++ project.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE project;
+
+CREATE TABLE airport_details(id int, airport_name varchar(20), airport_arrival int , airport_departure int, airport_price int);
+ USE project;
+ USE Airport_details
+ 
+ 
+ CREATE TABLE Cricket_team(id int, cricket_team_name varchar(20), no_of_palyer int, captain_name varchar(20), coach_name varchar(20));
+ 
